5e0e371f3c40fc5671c1ea73a553f5eb9301b33c
Alugbin Boluwatife Godwin <godwinalugbin004@gmail.com>
2022-07-02 14:51:30 +0100

    dev

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,20 @@
 import '../styles/globals.css'
 import AppProvider from '../components/Context/Context'
 import {useEffect} from "react"
-// appId: "bbf6acad-d4ad-4fee-9df5-d4e3f554f02f",
+// appId: "bbf6acad-d4ad-4fee-9df5-d4e3f554f02f", appId: "bbf6acad-d4ad-4fee-9df5-d4e3f554f02f",
 //appId: "b40b7cc7-13dc-4662-8b48-efa668f9b72a", production
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     window.OneSignal = window.OneSignal || [];
-    OneSignal.push(function () {
-        OneSignal.init({
-          appId: "bbf6acad-d4ad-4fee-9df5-d4e3f554f02f",
-            notifyButton: {
-                enable: true,
-            },
-
-            allowLocalhostAsSecureOrigin: true,
-        });
+    OneSignal.push(function() {
+      OneSignal.init({
+        appId: "2564d3a2-5313-4cdf-b5bf-238012f6b7cb",
+        safari_web_id: "",
+        notifyButton: {
+          enable: true,
+        },
+        subdomainName: "devilo",
+      });
     });
 
     return () => {
