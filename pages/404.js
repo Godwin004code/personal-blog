55a73f7a9200fc528250068aafdbc2f72f0f200b
Alugbin Boluwatife Godwin <godwinalugbin004@gmail.com>
2022-07-02 14:59:30 +0100

    deio

--- pages/404.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Custom404() {
-    return (
-        <div style={{textAlign: 'center'}}>
-        <h2>Lost? Go back to <Link to='/' >Home</Link></h2>
-        </div>
-    )
-}
